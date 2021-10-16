ed5b9b92218d14ebd0d4773b19c779be5737958b
lev <lev.domasnix2013@gmail.com>
2021-10-16 14:33:48 +0500

    Условные операторы

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,24 +1,15 @@
+import 'dart:io';
+
 void main() {
-  var number1 = [1, 2, 3];
-  var number2 = List.generate(5, (index) => index + 1);
-  var number3 = [0, ...number1, 4, 5];
-  var number4 = <int>[1, 2, 4];
-  List<int> number5 = [1, 2, 3];
+  print("Enter integer:");
 
-  var set = {9, 8, 9};
+  var number = int.parse(stdin.readLineSync() as String);
 
-  var map = <String, String>{'key1': 'value1', 'key2': 'value2'};
-
-  number1.add(5);
-
-  print(number1.length);
-  print(number1);
-
-  print(number2);
-
-  print(number3);
-
-  print(set);
-
-  print(map['key1']);
+  if (number < 5) {
+    print('less than five');
+  } else if (number == 6) {
+    print(6);
+  } else {
+    print('equal or greater than five');
+  }
 }
