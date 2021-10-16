90918957f3d7ba7a7a36786ebc7ecf6eaf184236
lev <lev.domasnix2013@gmail.com>
2021-10-16 13:52:33 +0500

    final и const

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,28 +1,8 @@
 void main() {
-  num number1 = 1.1;
+  const hexRedColor = 0xFFFF0000; // вычесляется сразу
+  //const hexRedColor = 0xFFFF0000.abs(); // вычесляется в runtime
 
-  double number2 = 1.1;
+  final hexRedColor2 = 0xFFFF0000.abs();
 
-  int number3 = 1;
-
-  var number4 = 1;
-
-  //Strings
-
-  var string = "string";
-  print(string);
-
-  var string1 = "string" ' ' "string2";
-  print(string1);
-
-  var interpol = 'Num: ${number3}';
-  print(interpol);
-
-  dynamic dynamicVar = 1;
-
-  dynamicVar = 'str';
-
-  dynamicVar = 1.2;
-
-  dynamicVar = true;
+  print(hexRedColor);
 }
