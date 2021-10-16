622d7b9261f0e5843775ae36fa9be4ff6035c68a
lev <lev.domasnix2013@gmail.com>
2021-10-16 13:44:08 +0500

    Типы данных

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,3 +1,28 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  num number1 = 1.1;
+
+  double number2 = 1.1;
+
+  int number3 = 1;
+
+  var number4 = 1;
+
+  //Strings
+
+  var string = "string";
+  print(string);
+
+  var string1 = "string" ' ' "string2";
+  print(string1);
+
+  var interpol = 'Num: ${number3}';
+  print(interpol);
+
+  dynamic dynamicVar = 1;
+
+  dynamicVar = 'str';
+
+  dynamicVar = 1.2;
+
+  dynamicVar = true;
 }
