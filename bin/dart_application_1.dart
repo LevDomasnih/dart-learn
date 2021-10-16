86291d92637cc2f5406e1f45505f00280839855a
lev <lev.domasnix2013@gmail.com>
2021-10-16 14:10:23 +0500

    DateTime

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,18 +1,5 @@
 void main() {
-  // int -> double | .toDouble()
-  var intNum = 1;
-  var doubleNum = intNum.toDouble();
+  var dateTime1 = DateTime.now();
 
-  print(intNum);
-  print(doubleNum);
-
-  // double -> int | .round()
-  var doubleNum1 = 1.1;
-  var intNum1 = doubleNum1.round();
-
-  print(doubleNum1);
-  print(intNum1);
-
-  // int -> string | .toString()
-  print(doubleNum1.toString());
+  print(dateTime1);
 }
